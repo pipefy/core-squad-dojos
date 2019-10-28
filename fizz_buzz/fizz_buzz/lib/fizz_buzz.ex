7b71e0ea812030980936f39207e5ecfd977f3d73
Diogo Beda <diogo.fbeda@gmail.com>
2019-10-28 16:37:18 -0300

    Throw errors when generate_sequence receives invalid params

--- fizz_buzz/fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/fizz_buzz/lib/fizz_buzz.ex
@@ -1,5 +1,9 @@
 defmodule FizzBuzz do
-  def generate_sequence(_start, _end) do
-    [1, 2]
+  def generate_sequence(num_start, num_end) when is_integer(num_start) and is_integer(num_end) do
+    {:ok, Enum.to_list(num_start..num_end)}
+  end
+
+  def generate_sequence(_num_start, _num_end) do
+    {:error, "Invalid params"}
   end
 end
--- fizz_buzz/fizz_buzz/test/fizz_buzz_test.exs
+++ fizz_buzz/fizz_buzz/test/fizz_buzz_test.exs
@@ -2,9 +2,22 @@ defmodule FizzBuzzTest do
   use ExUnit.Case
   doctest FizzBuzz
 
-  test "returns a numeric sequence defined by arguments limit" do
-    result = FizzBuzz.generate_sequence(1, 2)
+  test "returns an error when params are not integers" do
+    result_1 = FizzBuzz.generate_sequence("1", "5")
+    expected = {:error, "Invalid params"}
 
-    assert result == [1, 2]
+    assert result_1 == expected
+
+    result_1 = FizzBuzz.generate_sequence("1", 5)
+    assert result_1 == expected
+
+    result_1 = FizzBuzz.generate_sequence(false, 5)
+    assert result_1 == expected
+  end
+
+  test "returns a numeric sequence defined by arguments limit" do
+    result = FizzBuzz.generate_sequence(1, 5)
+
+    assert result == {:ok, [1, 2, 3, 4, 5]}
   end
 end
