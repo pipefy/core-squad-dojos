5cdbd0abc35167b18a6ac6ccbcbe2eead5caac9a
Fernando Gonçalves <fernando.goncalves@pipefy.com>
2019-11-25 10:13:01 -0300

    fernando

--- tennis_game/score/lib/score.ex
+++ tennis_game/score/lib/score.ex
@@ -1,2 +1,18 @@
 defmodule Score do
+
+  def process_score(game) do
+    process_one_score(game, %{
+      player_1: "0",
+      player_2: "0"
+    })  
+  end
+ 
+  defp process_one_score([], status), do: status
+  defp process_one_score([player | tail], status) do
+    cond do 
+      player == :player_1 -> 
+        new_score = "15" #calc_new_score(status[:player_1])
+        status |> Map.put(:player_1, new_score)
+    end
+  end
 end
--- tennis_game/score/test/score_test.exs
+++ tennis_game/score/test/score_test.exs
@@ -2,15 +2,20 @@ defmodule ScoreTest do
   use ExUnit.Case
   doctest Score
 
-  test "greets the world" do
-    assert Score.hello() == :world
-  end
-
-  test "inital state from game" do
+  test "initial state from game" do
     game = []
 
-    Score.process_score(game) == %{
+    assert Score.process_score(game) == %{
       player_1: "0",
+      player_2: "0"
+    }
+  end
+
+  test "game with one score" do
+    game = [:player_1]
+
+    assert Score.process_score(game) == %{
+      player_1: "15",
       player_2: "0"
     }
   end
