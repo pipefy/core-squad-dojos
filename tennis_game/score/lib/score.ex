3ef5beadf2ee32ead17026acde6784bf00e4913d
ronualdo maciel <raxmac@gmail.com>
2019-11-25 10:21:15 -0300

    adding reduce

--- tennis_game/score/lib/score.ex
+++ tennis_game/score/lib/score.ex
@@ -1,18 +1,27 @@
 defmodule Score do
-
   def process_score(game) do
     process_one_score(game, %{
       player_1: "0",
       player_2: "0"
-    })  
+    })
   end
- 
+
   defp process_one_score([], status), do: status
-  defp process_one_score([player | tail], status) do
-    cond do 
-      player == :player_1 -> 
-        new_score = "15" #calc_new_score(status[:player_1])
-        status |> Map.put(:player_1, new_score)
+
+  defp process_one_score([player | tail] = players, status) do
+    Enum.reduce(players, status, fn player, state ->
+      cond do
+        player == :player_1 ->
+          new_score = calc_new_score(state[:player_1])
+
+          status |> Map.put(:player_1, new_score)
+      end
+    end)
+  end
+
+  defp calc_new_score(previous_score) do
+    cond do
+      "15" -> "30"
     end
   end
 end
--- tennis_game/score/test/score_test.exs
+++ tennis_game/score/test/score_test.exs
@@ -6,17 +6,26 @@ defmodule ScoreTest do
     game = []
 
     assert Score.process_score(game) == %{
-      player_1: "0",
-      player_2: "0"
-    }
+             player_1: "0",
+             player_2: "0"
+           }
   end
 
   test "game with one score" do
     game = [:player_1]
 
     assert Score.process_score(game) == %{
-      player_1: "15",
-      player_2: "0"
-    }
+             player_1: "15",
+             player_2: "0"
+           }
+  end
+
+  test "game with two scores" do
+    game = [:player_1, :player_1]
+
+    assert Score.process_score(game) == %{
+             player_1: "30",
+             player_2: "0"
+           }
   end
 end
