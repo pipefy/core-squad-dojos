0358cc1d41ec738dec3ab77a7bae7bbf3d8270a1
Priscila Aranha <aranhaqg@gmail.com>
2019-11-25 10:30:56 -0300

    Add recursion to calc score.

--- tennis_game/score/lib/score.ex
+++ tennis_game/score/lib/score.ex
@@ -9,19 +9,23 @@ defmodule Score do
   defp process_one_score([], status), do: status
 
   defp process_one_score([player | tail] = players, status) do
-    Enum.reduce(players, status, fn player, state ->
+    new_status =
       cond do
         player == :player_1 ->
-          new_score = calc_new_score(state[:player_1])
+          new_score = calc_new_score(status[:player_1])
 
           status |> Map.put(:player_1, new_score)
       end
-    end)
+
+    IO.inspect(new_status)
+    process_one_score(tail, new_status)
   end
 
   defp calc_new_score(previous_score) do
-    cond do
+    case previous_score do
+      "0" -> "15"
       "15" -> "30"
+      "30" -> "40"
     end
   end
 end
